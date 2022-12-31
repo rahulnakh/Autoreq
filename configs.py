b36c0018c86a33f3fbe460f4aaed839facc2e20a
MR-BOTZt <120255966+MR-BOTZt@users.noreply.github.com>
2023-01-01 01:46:29 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
-    SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    API_ID = int(getenv("API_ID", "23980613"))
+    API_HASH = getenv("API_HASH", "b54b7945e223bded6baa22d0a7c72884")
+    BOT_TOKEN = getenv("BOT_TOKEN", "5841285654:AAE5mitHVoVcVCxjppZ14PeLJHZgGMGGqGA")
+    FSUB = getenv("FSUB", "ndjdmdjdjdndn)
+    CHID = int(getenv("CHID", "-1001894487652)
+    SUDO = list(map(int, getenv("5531461861 5809721859 ").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://evatgbottelegram:evatgbottelegram@cluster0.hckeuhb.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
